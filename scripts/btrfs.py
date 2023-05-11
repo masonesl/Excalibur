7e861469c4c13f856a3b9949657c81634702018e
Mason <mason@excalibur.lan>
2023-05-10 22:22:02 -0600

    Make btrfs subvolumes functional

--- scripts/btrfs.py
+++ scripts/btrfs.py
@@ -52,55 +52,79 @@ class Btrfs:
         subvolume_path: str,
         mountpoint: str,
         compression: str,
-        mount_options: list
+        mount_options: list,
+        target_mountpoint: str
     ):
 
-        create_subvol = lambda subvol_path : f"btrfs subvolume create {subvol_path}"
+        create_subvol = lambda subvol_path : f"btrfs subvolume create {target_mountpoint}/{subvol_path}"
         
         split_subvol = subvolume_path.strip("/").split("/")
         i = 0
         for vol in split_subvol:
             if vol not in self.subvolumes:
-                if i == 0:
-                    self.subvolumes.append(vol)
+                mp = comp = m_options = None
+                
+                if i == len(split_subvol) - 1:
+                    vol_to_add = subvolume_path
+                    mp = mountpoint
+                    comp = compression
+                    m_options = mount_options
+                elif i == 0:
                     vol_to_add = vol
                 else:
-                    vol_to_add = "/".join(vol[:i+1])
+                    vol_to_add = "/".join(split_subvol[:i+1])
                     
                 self.subvolumes[vol_to_add] = {
-                    "mountpoint" : mountpoint,
-                    "compression" : compression,
-                    "mount-options" : mount_options
+                    "mountpoint" : mp,
+                    "compression" : comp,
+                    "mount-options" : m_options
                 }
                 
-                cmd.execute(create_subvol(self.subvolumes.keys()[-1]), dry_run=self.dry_run)
+                cmd.execute(create_subvol(vol_to_add), dry_run=self.dry_run)
             
             i += 1
             
+    #--------------------------------------------------------------------------        
+    
+    def get_mountpoint(self, subvolume_path):
+        if subvolume_path in self.subvolumes:
+            return self.subvolumes[subvolume_path]["mountpoint"]
+        else:
+            return None
+            
+    #--------------------------------------------------------------------------
+            
     def mount_subvolume(
         self,
-        subvolume_path
+        subvolume_path: str,
+        override_mount: str
     ):
+        
+        if not self.get_mountpoint(subvolume_path):
+            return
         
         subvol = self.subvolumes[subvolume_path]
         
-        mount_command = f"mount -o subvol={subvol}"
+        mount_command = f"mount -m -o subvol={subvolume_path}"
         
         if compress := subvol["compression"]:
             mount_command += f",compress={compress}"
         
         if options := subvol["mount-options"]:
-            mount_command += ",".join(options)
+            mount_command += f",{','.join(options)}"
             
         mount_command += f" /dev/disk/by-uuid/{self.uuid}"
         
-        mount_command += f" {subvol['mountpoint']}"
+        mount_command += f" {override_mount}{self.get_mountpoint(subvolume_path)}"
         
         cmd.execute(mount_command, dry_run=self.dry_run)
+        
+    #--------------------------------------------------------------------------    
+    
+    def __repr__(self):
+        return_str = f"{self.__class__.__name__}("
+        for attribute in self.__dict__:
+            return_str += f"{attribute}={self.__dict__[attribute]}, "
+        return f"{return_str.strip(', ')})"
             
-        
-        
-
-
-
-        
+# EOF
